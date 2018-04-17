5f387881ec96e2249d35854f6516f4264e7a7568
shinespark <shinespark64@gmail.com>
2018-04-17 18:23:12 +0900

    end of "Define Your Data Model"

---
+++ FoodTracker2/Meal.swift
@@ -0,0 +1,34 @@
+//
+//  Meal.swift
+//  FoodTracker2
+//
+//  Created by metroid on 2018/04/17.
+//  Copyright © 2018 shinespark. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // Mark: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
